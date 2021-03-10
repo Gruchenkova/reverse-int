93de554159351023345b94e2a8f9edb53386839b
Evgenia Gruchenkova <gruchenkovaevgenia@yandex.by>
2021-03-10 10:35:49 +0300

    feat:add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+	let arr = Array.from(arguments);
+	if (arguments == undefined){
+		return 0
+	}else if(arr[0] < 0){
+		arr[0] = arr[0] * -1;
+	}
+		let b = arr[0] + '';
+		let newArr = b.split('').reverse().join("");
+		let result = +newArr;
+		return result;
 }
